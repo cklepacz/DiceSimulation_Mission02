c7ba8597daf1b4f092b5ea6506215f9ac51139b1
Cameron Klepacz <cameron.klepacz@gmail.com>
2024-01-17 13:32:16 -0700

    Add project files.

---
+++ DiceCalc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceSimulation
+{
+    internal class DiceCalc
+    {
+        //Make the countNumbers array public to ensure it can be called in the first class. It only holds 0 - 12, to represent the results of rolling 2 dice
+        public int[] countNumbers = new int[13];
+
+        //Create the RollDice method
+        public void RollDice(int count)
+        {
+            
+            //Create a new random number variable
+            Random rnd = new Random();
+
+            //Create an array called 'results' to store the results of each roll 
+            int[] results = new int[count];
+
+            //For each element in the 'results' array, calculate the result of rolling two dice (AKA a number between 2 and 12)
+            //Update the count of each number in the 'countNumber' array each time the 'results' array is updated
+            for (int i = 0; i < count; i++)
+            {
+                results[i] = rnd.Next(1, 7) + rnd.Next(1, 7);
+                countNumbers[results[i]]++;
+            }
+            for (int i = 2; i < countNumbers.Length; i++)
+            {
+                countNumbers[i] = (int)Math.Round((((double)countNumbers[i] / count) * 100), 0, MidpointRounding.AwayFromZero);
+                //System.Console.WriteLine($"Percentage of {i}: {countNumbers[i]}%"); <-- Just in case you want to see the percentage as a number
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using DiceSimulation;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        //New DiceCalc instance
+        DiceCalc dc = new DiceCalc();
+
+        //Establish count integer
+        int count = 0;
+
+        //Set count to user-input
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\nHow many rolls would you like to simulate?\n*NOTE: This simulates the throwing of 2 dice.");
+        count = int.Parse(System.Console.ReadLine());
+
+        //Call the RollDice method in the dc class instance and pass it the 'count' variable
+        dc.RollDice(count);
+
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS\nEach \"*\" represents 1% of the total number of rolls.\nTotal number of rolls = " + count + ".\n");
+
+        //Create a new string using the constructor to repeat the '*' character dc.countNumbers[i] number of times
+        for (int i = 2; i < dc.countNumbers.Length; i++)
+        {
+            System.Console.WriteLine($"{i}: {new string('*', dc.countNumbers[i])}");
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
